771cf2ceebc188e89a765b6cabed1b9140ad0eb7
Matt <themattsterr@gmail.com>
2015-02-08 16:08:04 -0500

    Modified new container script

--- Docker/newcontainer.sh
+++ Docker/newcontainer.sh
@@ -1,25 +1,32 @@
+
+currentPath=$(pwd)
+
 echo "Removing Container ID File"
-rm cidfile
+rm $currentPath/cidfile
 
 echo "Copying StudentFiles Contents"
-cp ~/docker/StudentFiles/* ~/docker/shared
+mkdir $currentPath/shared
+cp $currentPath/StudentFiles/* $currentPath/shared
 
 echo "Creating container"
-docker run -v ~/docker/shared:/shared ags-vm sleep 5
-docker ps -lq >> cidfile
+docker run -v $currentPath/shared:/shared ags-vm sleep 5
+docker ps -lq >> $currentPath/cidfile
 
 echo "Starting container"
-docker start $(cat cidfile)
+docker start $(cat $currentPath/cidfile)
 
 echo "Compiling Source Files"
-docker exec -i -t $(cat cidfile) gcc -o /shared/a.out /shared/test.c
+docker exec -i -t $(cat $currentPath/cidfile) gcc -o /shared/a.out /shared/test.c
 
 echo "Running binary file"
-docker exec $(cat cidfile) ./shared/a.out >> output
+docker exec $(cat $currentPath/cidfile) ./shared/a.out >> $currentPath/output.txt
 
 echo "Writing Differences"
-rm diff
-docker diff $(cat cidfile) | sed -e '/^C/d' -e 's:^A ::g' >> diff
+docker diff $(cat $currentPath/cidfile) | sed -e '/^C/d' -e 's:^A ::g' >> $currentPath/diff
 
-
-
+echo "Moving Files to Completed"
+mkdir $currentPath/completed
+mv $currentPath/diff $currentPath/completed
+mv $currentPath/shared/a.out $currentPath/completed
+mv $currentPath/output.txt $currentPath/completed
+rm -R $currentPath/shared
