dffd1cae1d6b226b3489a6c7c5a40f041397e379
Matt <themattsterr@gmail.com>
2015-02-09 17:33:19 -0500

    Conflict resolutions?

--- Meteor/ags/.meteor/local/build/programs/web.browser/app/client/lib/subscriptions.js
+++ Meteor/ags/.meteor/local/build/programs/web.browser/app/client/lib/subscriptions.js
@@ -3,5 +3,6 @@ Meteor.subscribe('availableCourses');
 Meteor.subscribe('currentUserInfo');
 Meteor.subscribe('userData');
 Meteor.subscribe('allAssignments');
+Meteor.subscribe('allAssignment');
 
 })();
--- Meteor/ags/client/controllers/mainContentInfo.js
+++ Meteor/ags/client/controllers/mainContentInfo.js
@@ -46,53 +46,7 @@ Template.mainContent.events({
 	},
 	'submit #createAssignment': function(event){
 		event.preventDefault();
-		var name = event.target.assignmentNameField.value;
-		var description = event.target.assignmentDescriptionField.value;
-		var dateAvailable = event.target.assignmentDateAvailableField.value;
-		var dateDue = event.target.assignmentDateDueField.value;
-		var points = event.target.assignmentPointsField.value;
 
-		var vtaReader = new FileReader();
-		var solutionReader = new FileReader();
-
-		var vta = event.target.assignmentVTAField.files[0];
-		var solution = event.target.assignmentSolutionField.files[0];
-		var inputFileList = event.target.assignmentInputField.files;
-
-		var vtaObj, solutionObj, inputObjList = [];
-/*
-		for(var i=0; i < inputFileList.length; i++) {
-			inputReaderList.push(new FileReader());
-			var input = inputFileList[i];
-			inputReader.readAsText(input);
-
-			inputObjList.push({name: input.name, contents:inputReader.result});
-		}*/
-
-/*		inputFileList.map(function(item, index, array){
-			var inputReader = new FileReader();
-			inputReader.onload = function(){
-				inputObjList.push({name: item.name, contents:inputReader.result});
-			}
-			inputReader.readAsText(item);
-		})*/
-
-
-
-		vtaReader.onloadend = function(){
-			vtaObj = {name: vta.name, contents:vtaReader.result};
-		}
-
-		solutionReader.onloadend = function(){
-			solutionObj = {name: solution.name, contents:solutionReader.result};
-		}
-		
-		vtaReader.readAsText(vta);
-		solutionReader.readAsText(solution);
-
-		while(vtaReader.readyState != vtaReader.DONE);
-
-		var currentCourseId = Session.get('currentCourse')._id;
 		Meteor.call('insertAssignmentData', currentCourseId, name, description, dateAvailable, dateDue, points, vtaObj, solutionObj, inputObjList);
 	},
 })
