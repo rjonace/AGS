a5d8a122ec41831b9074ff3a32a5b1a886022d37
Matt <themattsterr@gmail.com>
2015-02-20 14:12:28 -0500

    Redesign and renaming

--- Docker/java/execjavafiles.sh
+++ Docker/java/execjavafiles.sh
@@ -6,14 +6,19 @@ mkdir $currentPath/shared
 cp $currentPath/StudentFiles/* $currentPath/shared
 
 echo "Creating container"
+#start a timer
 docker run -v $currentPath/shared:/shared ags-vm sleep 5
 docker ps -lq >> $currentPath/cidfile
+#end that timer
 
 echo "Starting container"
 docker start $(cat $currentPath/cidfile)
 
 echo "Compiling Source Files"
+#start a timer
 docker exec -i -t $(cat $currentPath/cidfile) javac /shared/test.java
+#end that timer
 
 echo "Running binary file"
+#timer inside javaRunner
 docker exec $(cat $currentPath/cidfile) javaRunner.sh
