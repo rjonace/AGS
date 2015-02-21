f014f889748c6886b4c8636f5d30463f82c60bc3
Matt <themattsterr@gmail.com>
2015-02-20 19:43:09 -0500

    removed an echo

--- Docker/CompileTestJava/StudentFiles/javaRunner.sh
+++ Docker/CompileTestJava/StudentFiles/javaRunner.sh
@@ -36,7 +36,6 @@ cd /shared/
 
 for classfile in *.class; do
     classname=${classfile%.*}
-    echo $classname
 
     #Execute fgrep with -q option to not display anything on stdout when the match is found
     if javap -public $classname | fgrep -q 'public static void main(java.lang.String[])'; then
