103e500da6fb47392f9a90eff59faa8dcd67e9ed
Matt <themattsterr@gmail.com>
2015-02-21 15:01:59 -0500

    Changed javaCompiler to compile + execute

--- Docker/CompileTestJava/StudentFiles/javaCompiler.sh
+++ Docker/CompileTestJava/StudentFiles/javaCompiler.sh
@@ -7,3 +7,11 @@ for javaFile in *.java; do
     javac ${javaFile}
 
 done
+
+for classfile in *.class; do
+    classname=${classfile%.*}
+
+    if javap -public $classname | fgrep -q 'public static void main(java.lang.String[])'; then
+        java $classname >> output.txt
+    fi
+done
--- Docker/CompileTestJava/execjavafiles.sh
+++ Docker/CompileTestJava/execjavafiles.sh
@@ -16,10 +16,10 @@ echo "Starting container"
 docker start $(cat $currentPath/cidfile)
 
 echo "Compiling Source Files"
-docker exec -i -t $(cat $currentPath/cidfile) sh /shared/javaCompiler.sh
+docker exec $(cat $currentPath/cidfile) sh /shared/javaCompiler.sh
 
-echo "Running binary file"
-docker exec $(cat $currentPath/cidfile) sh /shared/javaRunner.sh
+#echo "Running binary file"
+#docker exec $(cat $currentPath/cidfile) sh /shared/javaRunner.sh
 
 echo "Writing Differences"
 docker diff $(cat $currentPath/cidfile) | sed -e '/^C/d' -e 's:^A ::g' >> $currentPath/$1/diff
