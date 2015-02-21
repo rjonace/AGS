bd5a8b04b10a7e8cfeabb17f541090d10b574ec8
Matt <themattsterr@gmail.com>
2015-02-21 15:20:19 -0500

    That last commit was almost it adding an echo

--- Docker/CompileTestJava/StudentFiles/javaCompiler.sh
+++ Docker/CompileTestJava/StudentFiles/javaCompiler.sh
@@ -12,6 +12,7 @@ for classfile in *.class; do
     classname=${classfile%.*}
 
     if javap -public $classname | fgrep -q 'public static void main(java.lang.String[])'; then
+        echo $classname
         java $classname
     fi
 done
