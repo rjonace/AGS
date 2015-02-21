e0d0ca5957f58fc598f7d0aa81db6cc901521f46
Matt <themattsterr@gmail.com>
2015-02-21 15:22:37 -0500

    Another echo

--- Docker/CompileTestJava/StudentFiles/javaCompiler.sh
+++ Docker/CompileTestJava/StudentFiles/javaCompiler.sh
@@ -7,10 +7,10 @@ for javaFile in *.java; do
     javac ${javaFile}
 
 done
-
+echo "Compiled"
 for classfile in *.class; do
     classname=${classfile%.*}
-
+    echo $classfile
     if javap -public $classname | fgrep -q 'public static void main(java.lang.String[])'; then
         echo $classname
         java $classname
