81c28e1634e4425a1555fb4e66e0f09116f6b93f
Matt <themattsterr@gmail.com>
2015-02-22 21:03:06 -0500

    call to apply

--- Meteor/ags/client/controllers/mainContentInfo.js
+++ Meteor/ags/client/controllers/mainContentInfo.js
@@ -151,7 +151,7 @@ Template.mainContent.events({
 				}
 		);*/
 
-		var folderName = Meteor.call('prepareGrade', currentUserId, currentAssignment._id, submission, filePath);
+		var folderName = Meteor.apply('prepareGrade', [currentUserId, currentAssignment._id, submission, filePath],true);
 		Meteor.apply('writeSubmissionFiles', [submission, filePath + "/" + folderName] , true);
 		Meteor.apply('writeInstructorFiles', [currentAssignment, filePath + "/" + folderName], true);
 		Meteor.apply('gradeSubmission', [submission, filePath, folderName, currentUserId, currentAssignment._id] , true);
