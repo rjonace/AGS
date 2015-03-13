ee332315f25d311896ddca93e720d6e79f92041e
William Adkins <wadkins@knights.ucf.edu>
2015-03-13 14:58:56 -0400

    Added to gradeTest

--- gradeTest/VTA.h
+++ gradeTest/VTA.h
@@ -1,4 +1,18 @@
+#ifndef VTA_H
+#define VTA_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
+typedef struct { 
+	const char* (* const displayScore)(int, char *);
+	const char* (* const display)(void);
+} namespace_struct;
+
+extern namespace_struct const VTA;
+
+
+
+#endif
