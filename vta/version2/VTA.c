41d827b5e64abe473b83a252f3ec6566fb9ae9b0
Rodney Jonace <rjonace@gmail.com>
2015-03-21 13:20:28 -0400

    finish VTA function descriptions

--- vta/version2/VTA.c
+++ vta/version2/VTA.c
@@ -1,52 +1,5 @@
 #include "VTA.h"
 
-/** Automatically checks whether stdin was used. Adds/deducts points based on compliance */
-void addGradeUseStdin(int points);
-
-/** Automatically checks whether stdout was used. Adds/deducts points based on compliance */
-void addGradeUseStdout(int points);
-
-/** Adds an entry to the Code Points of the results object that must be manually graded by the TA */
-void addCodePoints(const char* description, int points);
-
-/** Returns the total number of instructor created input files that are available;
- *  Implementation idea: Maybe just put all of the input files in the a directory and run the 
- *  command: "ls -1 | wc" on it */
-int numInputFiles(void);
-
-/** Assuming there is one and only one input file, this returns its contents as a string */
-char* getInputFromFile(void);
-
-/** Returns the contents of file with inputNumber as a string */
-char* getInputFromFiles(int inputNumber);
-
-/** Returns a string that represents the contents of an input file containing numCases of input cases,
- *  each of which follows the pattern in the first argument 
- *  Implementation idea: probably should make the first line the number of cases, and make the beginning of
- *  each case contain the number of lines for that case */ 
-char* generateInputFile(const char* pattern, int numCases);
-
-/** Runs either the instructor's of student's compiled binary and returns its stdout as a string; "mode"
- *  determines which should be run 
- *  Implementation idea:  Per Professor Heinrch's suggestion, we should probably return an entire 
- *  struct of information about how the file ran---not just the output */
-char* run(char mode);
-
-/** Runs either the instructor's of student's compiled binary with the contents of "input" piped in and 
- *  returns the stdout as a string; "mode" determines which should be run 
- *  Implementation idea:  Per Professor Heinrch's suggestion, we should probably return an entire 
- *  struct of information about how the file ran---not just the output */
-char* runWithInput(char mode, char* input);
-
-score_struct* compareOutputsByLine(const char* correct_output, const char* student_output, int total_points);
-score_struct* compareOutputsByLineRegex(const char* regex, const char* correct_output, const char* student_output, int total_points);
-score_struct* compareOutputsByCase(int case_lines, const char* correct_output, const char* student_output, int total_points);
-score_struct* compareOutputsByCaseRegex(int case_lines, const char* regex, const char* correct_output, const char* student_output, int total_points);
-void addExecResults(int input_file_num, const char* input, const char* correct_output, const char* student_output, score_struct scores[]);
-
-void addCheckHeader(const char* regex, int points);
-void addStylePoints(const char* description, int points);
-
 VTA_namespace const VTA = {
 	addGradeUseStdin,
 	addGradeUseStdout,
@@ -116,22 +69,26 @@ score_struct* compareOutputsByLine(const char* correct_output, const char* stude
 	return NULL;
 }
 
-score_struct* compareOutputsByLineRegex(const char* regex, const char* correct_output, const char* student_output, int total_points)
+score_struct* compareOutputsByLineRegex(const char* regex, const char* correct_output, 
+	const char* student_output, int total_points)
 {
 	return NULL;
 }
 
-score_struct* compareOutputsByCase(int case_lines, const char* correct_output, const char* student_output, int total_points)
+score_struct* compareOutputsByCase(int case_lines, const char* correct_output, const char* student_output, 
+	int total_points)
 {
 	return NULL;
 }
 
-score_struct* compareOutputsByCaseRegex(int case_lines, const char* regex, const char* correct_output, const char* student_output, int total_points)
+score_struct* compareOutputsByCaseRegex(int case_lines, const char* regex, const char* correct_output, 
+	const char* student_output, int total_points)
 {
 	return NULL;
 }
 
-void addExecResults(int input_file_num, const char* input, const char* correct_output, const char* student_output, score_struct scores[])
+void addExecResults(int input_file_num, const char* description, const char* input, 
+	const char* correct_output, const char* student_output, score_struct scores[])
 {
 
 }
--- vta/version2/VTA.h
+++ vta/version2/VTA.h
@@ -32,4 +32,86 @@ typedef struct {
 
 extern VTA_namespace const VTA;
 
+/** Automatically checks whether stdin was used. 
+ *
+ *  Adds/deducts points to Code Points object based on compliance */
+void addGradeUseStdin(int points);
+
+/** Automatically checks whether stdout was used
+ *  
+ *  Adds/deducts points to Code Points object based on compliance */
+void addGradeUseStdout(int points);
+
+/** Adds an entry to the Code Points of the results object that must be manually graded by the TA */
+void addCodePoints(const char* description, int points);
+
+/** Returns the total number of instructor created input files that are available;
+ *  Implementation idea: Maybe just put all of the input files in the a directory and run the 
+ *  command: "ls -1 | wc" on it */
+int numInputFiles(void);
+
+/** Assuming there is one and only one input file, this returns its contents as a string */
+char* getInputFromFile(void);
+
+/** Returns the contents of file with inputNumber as a string */
+char* getInputFromFiles(int inputNumber);
+
+/** Returns a string that represents the contents of an input file containing numCases of input cases,
+ *  each of which follows the pattern in the first argument 
+ *  Implementation idea: probably should make the first line the number of cases, and make the beginning of
+ *  each case contain the number of lines for that case */ 
+char* generateInputFile(const char* pattern, int numCases);
+
+/** Runs either the instructor's or student's compiled binary and returns its stdout as a string; "mode"
+ *  determines which should be run 
+ *  Implementation idea:  Per Professor Heinrch's suggestion, we should probably return an entire 
+ *  struct of information about how the file ran---not just the output */
+char* run(char mode);
+
+/** Runs either the instructor's or student's compiled binary with the contents of "input" piped in and 
+ *  returns the stdout as a string; "mode" determines which should be run 
+ *  Implementation idea:  Per Professor Heinrch's suggestion, we should probably return an entire 
+ *  struct of information about how the file ran---not just the output */
+char* runWithInput(char mode, char* input);
+
+/** Does a diff comparison of the text in correct_output and student_output, treating each line as a case,
+ *  and distributes the total_points evenly by number of cases
+ *
+ *  Returns an array of score_struct */
+score_struct* compareOutputsByLine(const char* correct_output, const char* student_output, int total_points);
+
+/** Does a diff comparison of the results of regex capture of the text in correct_output and student_output, 
+ *  treating each line as a case, and distributes the total_points evenly by number of cases
+ *
+ *  Returns an array of score_struct */
+score_struct* compareOutputsByLineRegex(const char* regex, const char* correct_output, 
+	const char* student_output, int total_points);
+
+/** Does a diff comparison of the text in correct_output and student_output, treating cases as groups of 
+ *  lines of length case_lines, and distributes the total_points evenly by number of cases
+ *
+ *  Returns an array of score_struct */
+score_struct* compareOutputsByCase(int case_lines, const char* correct_output, const char* student_output, 
+	int total_points);
+
+/** Does a diff comparison of the results of regex capture of the text in correct_output and student_output,
+ *  treating cases as groups of lines of length case_lines, and distributes the total_points evenly by number
+ *  of cases
+ *
+ *  Returns an array of score_struct */
+score_struct* compareOutputsByCaseRegex(int case_lines, const char* regex, const char* correct_output, 
+	const char* student_output, int total_points);
+
+/** Adds an entry to the Execution Points of the results object with the results of the scores array */
+void addExecResults(int input_file_num, const char* description, const char* input, 
+	const char* correct_output, const char* student_output, score_struct scores[]);
+
+/** Checks whether header comment of student source code matches regex */
+void addCheckHeader(const char* regex, int points);
+
+/** Adds an entry to the Style Points of the results object that must be manually graded by the TA 
+ *
+ *  Adds/deducts points to Code Points object based on compliance */
+void addStylePoints(const char* description, int points);
+
 #endif
