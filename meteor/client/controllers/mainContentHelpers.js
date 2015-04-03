f5cfc949912c726ad2dca9bc858bc5317e69c1b0
Matt <themattsterr@gmail.com>
2015-04-03 18:59:17 -0400

    moved icon in inputs table

--- meteor/client/controllers/mainContentHelpers.js
+++ meteor/client/controllers/mainContentHelpers.js
@@ -159,7 +159,7 @@ Template.mainContent.helpers({
 						HTMLString += '<td>' + tabObj["gradedInputs"][r].name + '</td>'
 						HTMLString += '<td>' + tabObj["gradedInputs"][r].pointsPossible + '</td>'
 						HTMLString += '<td>' + tabObj["gradedInputs"][r].pointsEarned + '</td>'
-						HTMLString += '<td>' + tabObj["gradedInputs"][r].comments + '</td>'
+						HTMLString += '<td>' + tabObj["gradedInputs"][r].comments + '<i class="drop down icon"></i></td>'
 						HTMLString += '</tr>'
 						HTMLString += '<tr><td colspan="4" style="display: none;"><table class="ui celled table"><thead><tr><th>test case #</th><th>correct output</th><th>student output</th><th>points</th><th>comments</th></tr></thead><tbody>'
 						for (var caseIndex in tabObj["gradedInputs"][r].cases){
@@ -171,7 +171,7 @@ Template.mainContent.helpers({
 							HTMLString += '<td>' + tabObj["gradedInputs"][r].cases[caseIndex].correctOutput + '</td>'
 							HTMLString += '<td>' + tabObj["gradedInputs"][r].cases[caseIndex].studentOutput + '</td>'
 							HTMLString += '<td>' + tabObj["gradedInputs"][r].cases[caseIndex].points + '</td>'
-							HTMLString += '<td>' + tabObj["gradedInputs"][r].cases[caseIndex].comments + '<i class="drop down icon"></i></td>'
+							HTMLString += '<td>' + tabObj["gradedInputs"][r].cases[caseIndex].comments + '</td>'
 							HTMLString += '</td></tr>'
 						}
 						HTMLString += '</tbody></table></tr>'
