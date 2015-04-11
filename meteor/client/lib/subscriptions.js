0dc9fb8a6b7bea4f931e446552efa52b738d5ee3
Matt <themattsterr@gmail.com>
2015-04-11 17:08:16 -0400

    moved callbacks into a callback

--- meteor/client/lib/subscriptions.js
+++ meteor/client/lib/subscriptions.js
@@ -8,6 +8,38 @@ Meteor.subscribe('submissionData');
 
 Tracker.autorun(function() {
 	if(Session.get('manGradedRow')){
+	    var curRow = Session.get('manGradedRow');
+	    console.log(curRow);
+	    $('#viewFilesModal').modal({ onVisible: function(){
+            $('#viewFilesModal').modal({
+                onDeny : function() {
+                    console.log('Deny');
+                    Session.set('manGradedRow', null);
+                },
+                onApprove : function() {
+                    curRow.pointsEarned = Number($('#pointsEarnedInput').val());
+                    curRow.comments = $('#commentsInput').val();
+                    var updatedFeedback = submission.feedbackObj;
+                    updatedFeedback["sections"][tableIndex]["rows"][rowIndex] = curRow;
+                    Meteor.call('updateFeedbackObj', submission.id_Student, Session.get('currentAssignment')._id, submission.subNumber, updatedFeedback, 
+                        function(error, result) {
+                            if (!error)
+                                Meteor.call('resetSubmissionSession', submission.id_Student, Session.get('currentAssignment')._id, submission,
+                                    function(error,result){
+                                        if(!error){
+                                            Session.set('currentSubmission',result);
+                                        }
+                                        else console.log(error);
+                                    }
+                                );
+                            else
+                                console.log(error);
+                        }
+                    );
+                    Session.set('manGradedRow', null);
+                }
+            });
+	    }});
 
 		console.log('if');
 	} else {
@@ -15,3 +47,5 @@ Tracker.autorun(function() {
 		console.log('else');
 	}
 }, {onError: function(error){ console.log(error); }})
+
+
