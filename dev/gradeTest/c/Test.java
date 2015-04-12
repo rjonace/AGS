ba75a5e7ad882dd8897f90b37316d4e9a73d8908
Rodney <rjonace@knights.ucf.edu>
2015-04-12 16:57:54 -0400

    VTA java json test

--- dev/gradeTest/c/Test.java
+++ dev/gradeTest/c/Test.java
@@ -1,5 +1,5 @@
-//import VTA;
 import java.io.*;
+import vta.VTA;
 
 public class Test 
 {
@@ -84,6 +84,36 @@ public class Test
 	public static void main(String[] args) throws IOException
 	{
 //		System.out.println(run('i'));
-		System.out.println(runWithInput('s', "hopscotch.in"));
+//		System.out.println(runWithInput('s', "hopscotch.in"));
+int j = 1;
+		VTA vta = new VTA();
+System.out.println(j++);
+		vta.addSection("Code Points");
+System.out.println(j++);
+		vta.addSection("Execution Points");
+System.out.println(j++);		
+		
+		vta.addManuallyGradedRow("Code Points", "Uses stdin", 10, "Edit this comment");
+System.out.println(j++);		
+		vta.addManuallyGradedRow("Code Points", "Uses stdout", 10, "Edit this comment");
+System.out.println(j++);		
+
+System.out.println(vta.addAutoGradedInput("Execution Points", "The hard one", "basketballgame.in"));
+System.out.println(j++);		
+		int numCases = 10;
+		// studentoutput, instructoroutput
+		int counter = 0;
+		for (int i = 0; i < numCases; i++) {
+			vta.addInputCase("Execution Points", "The hard one", 
+				counter % 2 == 0 ? "corr" + i : "same",
+				counter++ % 2 == 0 ? "stud" + i : "same",
+				50 / numCases,
+				"good job"
+			);
+System.out.println(j++);		
+		}
+	
+		vta.cleanUp();		
+System.out.println(j++);		
 	}
 }
