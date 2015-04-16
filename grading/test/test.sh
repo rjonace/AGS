806326a76cdf34dd07032220552b57b21fe8985b
Matt <themattsterr@gmail.com>
2015-04-15 20:42:10 -0400

    scripts for grading

--- grading/test/test.sh
+++ grading/test/test.sh
@@ -1,12 +1,11 @@
 #version 2 of autograder (non-skeleton)
-#create Instructor solution executable (execi)
+
 #create Autograder executable (name of file given)
 #store executables in assignment specific directory
 
-AG_NAME=$1
+ASS_ID=$1
+CURRENT_PATH=$2
 
-for classfile in *.class; do
-    classname=${classfile%.*}
-    COND=`javap -public $classname | fgrep 'public static void main(java.lang.String[])'`
-    echo $COND
-done
+echo "Creating Assignment Folder"
+mkdir $CURRENT_PATH/$ASS_ID
+
