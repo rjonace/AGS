66dc4d6fb408ca40a98da22173599c0f957fd092
rjonace <rjonace@knights.ucf.edu>
2015-04-18 03:11:27 -0400

    oh well

--- grading/createInstructorSolutionC.sh
+++ grading/createInstructorSolutionC.sh
@@ -10,6 +10,4 @@ COMPFLAGS=$3
 cd "$CURRENT_PATH/$ASS_ID/solution_files"
 pwd
 echo `pwd`
-for cFile in *.c; do
-	gcc $cFile -std=gnu99 -o execi #"$COMPFLAGS"
-done
+gcc $cFile -std=gnu99 -o execi "$COMPFLAGS"
