fe4a6b1aa035d89cc4a3d6a5123915bb10fd33b5
Matt <themattsterr@gmail.com>
2015-04-18 19:25:24 -0400

    hardcoded name

--- grading/createInstructorSolutionC.sh
+++ grading/createInstructorSolutionC.sh
@@ -1,14 +1,11 @@
 #create Autograder executable (name of file given)
 #store executables in assignment specific directory
 
-echo whatTheFuck
-
 ASS_ID=$1
 CURRENT_PATH=$2
 COMPFLAGS=$3
 
 cd "$CURRENT_PATH/$ASS_ID/solution_files"
 pwd
-touch testfile
 ls -GlRh --full-time
-gcc *.c -std=gnu99 -o execi "$COMPFLAGS"
+gcc basketballgame.c -std=gnu99 -o execi "$COMPFLAGS"
