4f78cbaddcde654392320b68ff02b4b58e2525ba
rjonace <rjonace@knights.ucf.edu>
2015-04-18 22:11:15 -0400

    debugging statements in create Autograder.sh

--- grading/createAutograderAPIversion.sh
+++ grading/createAutograderAPIversion.sh
@@ -5,6 +5,12 @@ ASS_ID=$1
 CURRENT_PATH=$2
 COMPFLAGS=$3
 
-cd "$CURRENT_PATH/$ASS_ID/autograder_files"
+cd "$CURRENT_PATH/$ASS_ID/autograder_files
+pwd
+ls
 javac $COMPFLAGS *.java
+pwd
+ls
 bash ../../../../createJar.sh Autograder.jar
+pwd
+ls
