c606a557a788a68b5e6cd90008ee5d86c4e42b42
rjonace <rjonace@knights.ucf.edu>
2015-04-18 23:05:19 -0400

    no quotes

--- grading/createAutograderAPIversion.sh
+++ grading/createAutograderAPIversion.sh
@@ -6,6 +6,6 @@ CURRENT_PATH=$2
 COMPFLAGS=$3
 
 cd "$CURRENT_PATH/$ASS_ID/autograder_files"
-echo "$COMPFLAGS"
-javac "$COMPFLAGS" *.java
+echo $COMPFLAGS
+javac $COMPFLAGS *.java
 bash ../../../../createJar.sh Autograder.jar
