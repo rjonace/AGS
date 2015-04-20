776c0a253b1925d5399e3d9061f5ddb296f8b543
rjonace <rjonace@knights.ucf.edu>
2015-04-20 15:19:23 -0400

    make createJar include all *.class files

--- grading/createJar.sh
+++ grading/createJar.sh
@@ -6,13 +6,13 @@ VTA_PATH=$2
 #compiles all .java
 javac *.java
 
-#creates jar file 
+#creates jar file packaging every class file in the directory
 #should only be one java file that has a main because that becomes the entry point of the jar
 for classfile in *.class; do
     classname=${classfile%.*}
     COND=`javap -public $classfile | fgrep 'public static void main(java.lang.String[])'`
     if [ -n "$COND" ]; then
-        jar cfe $JAR_NAME $classname $classfile
+        jar cfe $JAR_NAME $classname *.class $classfile
     fi
 done
 
