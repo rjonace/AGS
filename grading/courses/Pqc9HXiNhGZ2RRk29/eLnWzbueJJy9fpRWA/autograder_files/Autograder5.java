50412678b223a747ec5710b96dbf538cd3250477
student <rjonace@gmail.com>
2015-04-20 15:22:16 -0400

    test courses from server

---
+++ grading/courses/Pqc9HXiNhGZ2RRk29/eLnWzbueJJy9fpRWA/autograder_files/Autograder5.java
@@ -0,0 +1,45 @@
+import vta.VTA;
+
+class Thing {
+	public static void talk()
+	{
+		System.out.println("Hello world.");
+	}
+}
+
+public class Autograder5
+{
+	public static void main(String[] args)
+	{
+		VTA vta = new VTA("Java");
+		
+		vta.addSection("Code Points");
+		vta.addManuallyGradedRow("Code Points", "Uses stdin", 10, "Edit this comment");
+		vta.addManuallyGradedRow("Code Points", "Uses stdout", 10, "Edit this comment");
+
+		vta.addSection("Execution Points");
+		String[][] inputs = {
+			{"The hard one", "hopscotch.in"},
+			{"The easy one", "hopscotch_easy.in"}
+		};
+
+		for (String[] inputInfo : inputs) {
+			String inputDescription = inputInfo[0];
+			String inputFilename = inputInfo[1];
+
+			vta.runWithInput('i', inputFilename);
+			vta.runWithInput('s', inputFilename);
+			vta.parseCases(inputFilename, 1);
+
+			vta.addAutoGradedInput("Execution Points", inputDescription, inputFilename);
+			for(int i = 0; i < vta.correctAnswers.length; i++){
+				boolean correct = vta.correctAnswers[i].equals(vta.studentAnswers[i]);
+				vta.addInputCase("Execution Points", inputDescription, vta.correctAnswers[i], 
+					vta.studentAnswers[i], correct, 5, correct ? "Yeah good job" : "suckaahhhh!");
+			}
+		}
+
+		Thing.talk();
+		vta.cleanUp();
+	}
+}
