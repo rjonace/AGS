78b1dd52877c971bb89c5c6654b7b17a36644edd
rjonace <rjonace@knights.ucf.edu>
2015-04-22 09:03:49 -0400

    stuff

--- grading/createAutograderAPIversion.sh
+++ grading/createAutograderAPIversion.sh
@@ -8,5 +8,5 @@ VTA_PATH=$3
 echo Create Autograder thinks vta is at $VTA_PATH
 
 cd "$CURRENT_PATH/$ASS_ID/autograder_files"
-javac -classpath $VTA_PATH *.java
+javac -classpath .:$VTA_PATH *.java
 bash ../../../../createAGJar.sh Autograder.jar $VTA_PATH
