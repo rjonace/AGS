efed4813763e04c86aa20eae82de358aad2577ce
Rodney Jonace <rjonace@gmail.com>
2015-04-22 16:51:09 -0400

    suppress errors about empty directories in scripts

--- grading/createAndGradeStudentExecutableJava.sh
+++ grading/createAndGradeStudentExecutableJava.sh
@@ -1,11 +1,19 @@
 set -e
-SUB_PATH=$1
 
+SUB_PATH=$1
 cd $SUB_PATH
+
 mkdir -p ../student_files/
+touch ../student_files/dummy
 cp ../student_files/* .
+
 bash ../../../../createJar.sh execs
+
 cp ../autograder_files/Autograder.jar .
 cp ../solution_files/execi .
+
+mkdir -p ../input_files/
+touch ../input_files/dummy
 cp ../input_files/* .
+
 java -jar Autograder.jar
