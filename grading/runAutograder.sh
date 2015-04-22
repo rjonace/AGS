d70d92bca04b4551276a208d046e82687dd27671
rjonace <rjonace@knights.ucf.edu>
2015-04-21 21:58:25 -0400

    trying something else

--- grading/runAutograder.sh
+++ grading/runAutograder.sh
@@ -1,5 +1,5 @@
 CUR_PATH=$1
-
+echo trying runAutograder
 cd $CUR_PATH
-docker run -v $CUR_PATH:/shared/ ags-vm java -jar Autograder.jar
-#java -jar Autograder.jar
+#docker run -v $CUR_PATH:/shared/ ags-vm java -jar Autograder.jar
+java -jar Autograder.jar
